79b6d1a7c39cf2c22bcdfbf08d3bba8942af87bc
Chase <chase.leimbach@selu.edu>
2022-09-25 18:37:39 -0500

    Comment Entity
    - Completed

---
+++ Web/LearningStarter/Entities/Comment.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+
+namespace LearningStarter.Entities
+{
+    public class Comment
+    {
+        public int Id { get; set; }
+        public int UserId { get; set; }
+        public User User { get; set; }
+        public DateTimeOffset CreatedAt { get; set; }
+    }
+    public class CommentGetDto
+    {
+        public int Id { get; set; }
+        public int UserId { get; set; }
+        public DateTimeOffset CreatedAt { get; set; }
+    }
+    public class CommentCreateDto
+    {
+        public int UserId { get; set; }
+        public DateTimeOffset CreatedAt { get; set; }
+    }
+    public class CommentUpdateDto
+    {
+        public int UserId { get; set; }
+        public DateTimeOffset CreatedAt { get; set; }
+    }
+
+    public class CommentListingDto
+    {
+        public int UserId { get; set; }
+        public List<User> Users { get; set; } = new List<User>(); 
+        public DateTimeOffset CreatedAt { get; set; }
+    }
+}
