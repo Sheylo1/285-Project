3a5c9dd09bdba0dd64e07c1f831a8587f27ba9b8
Chase <chase.leimbach@selu.edu>
2022-09-26 20:26:17 -0500

    Updated Listing Parts
    - I think its fixed

--- Web/LearningStarter/Entities/Comment.cs
+++ Web/LearningStarter/Entities/Comment.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.Extensions.Hosting;
+using System;
 using System.Collections.Generic;
 using System.Data;
 
@@ -7,8 +8,6 @@ namespace LearningStarter.Entities
     public class Comment
     {
         public int Id { get; set; }
-        public int UserId { get; set; }
-        public User User { get; set; }
         public DateTimeOffset CreatedAt { get; set; }
         public string CommentText { get; set; }
         public List<Post> Posts { get; set; } = new List<Post>();
@@ -16,24 +15,23 @@ namespace LearningStarter.Entities
     public class CommentGetDto
     {
         public int Id { get; set; }
-        public int UserId { get; set; }
         public DateTimeOffset CreatedAt { get; set; }
+        public string CommentText { get; set; }
     }
     public class CommentCreateDto
     {
-        public int UserId { get; set; }
         public DateTimeOffset CreatedAt { get; set; }
+        public string CommentText { get; set; }
     }
     public class CommentUpdateDto
     {
-        public int UserId { get; set; }
         public DateTimeOffset CreatedAt { get; set; }
+        public string CommentText { get; set; }
     }
 
     public class CommentListingDto
     {
-        public int UserId { get; set; }
-        public List<User> Users { get; set; } = new List<User>(); 
         public DateTimeOffset CreatedAt { get; set; }
+        public string CommentText { get; set; }
     }
 }
