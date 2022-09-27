cadf037bf615f0990db4557d8001e385586fd2f7
Chase <chase.leimbach@selu.edu>
2022-09-26 20:31:50 -0500

    Updated Listing Parts
    - also removed/added changes from db

--- Web/LearningStarter/Entities/Social.cs
+++ Web/LearningStarter/Entities/Social.cs
@@ -8,8 +8,6 @@ namespace LearningStarter.Entities
         public int Id { get; set; }
         public int PostId { get; set; }
         public Post Post { get; set; }
-        public int CommentId { get; set; }
-        public Comment Comment { get; set; }
         public int Notifications { get; set; }
         public int Reminders { get; set; }
 
@@ -19,7 +17,6 @@ namespace LearningStarter.Entities
     {
         public int Id { get; set; }
         public int PostId { get; set; }
-        public int CommentId { get; set; }
         public int Notifications { get; set; }
         public int Reminders { get; set; }
     }
@@ -27,7 +24,6 @@ namespace LearningStarter.Entities
     public class SocialCreateDto
     {
         public int PostId { get; set; }
-        public int CommentId { get; set; }
         public int Notifications { get; set; }
         public int Reminders { get; set; }
     }
@@ -35,7 +31,6 @@ namespace LearningStarter.Entities
     public class SocialUpdateDto
     {
         public int PostId { get; set; }
-        public int CommentId { get; set; }
         public int Notifications { get; set; }
         public int Reminders { get; set; }
     }
@@ -43,9 +38,6 @@ namespace LearningStarter.Entities
     public class SocialListingDto
     {
         public int PostId { get; set; }
-        public List<Post> Posts { get; set; } = new List<Post>();
-        public int CommentId { get; set; }
-        public List<Comment> Comments { get; set; } = new List<Comment>();
         public int Notifications { get; set; }
         public int Reminders { get; set; }
     }
