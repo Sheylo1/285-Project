ecd1e53b0967c3609e5ecf7dcca6a990059548ee
Chase <chase.leimbach@selu.edu>
2022-09-26 20:05:08 -0500

    Updated Listing Parts
    - Should be fixed

--- Web/LearningStarter/Entities/Post.cs
+++ Web/LearningStarter/Entities/Post.cs
@@ -1,47 +1,41 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.OracleClient;
 
 namespace LearningStarter.Entities
 {
     public class Post
     {
         public int Id { get; set; }
-        public int UserId { get; set; }
-        public User User { get; set; }
         public DateTimeOffset CreatedAt { get; set; }
         public int CommentId { get; set; }
         public Comment Comment { get; set; }
-        
+        public List<Social> Socials { get; set; } = new List<Social>();
+
     }
 
     public class PostGetDto
     {
         public int Id { get; set; }
-        public int UserId { get; set; }
         public DateTimeOffset CreatedAt { get; set; }
         public int CommentId { get; set; }
     }
 
     public class PostCreateDto
     {
-        public int UserId { get; set; }
         public DateTimeOffset CreatedAt { get; set; }
         public int CommentId { get; set; }
     }
 
     public class PostUpdateDto
     {
-        public int UserId { get; set; }
         public DateTimeOffset CreatedAt { get; set; }
         public int CommentId { get; set; }
     }
 
     public class PostListingDto
     {
-        public int UserId { get; set; }
-        public List<User> Users { get; set; } = new List<User>();
         public DateTimeOffset CreatedAt { get; set; }
         public int CommentId { get; set; }
-        public List<Comment> Comments { get; set; } = new List<Comment>();
     }
 }
