a0d5fa491cc712279e3f2e2d68533c596fd54734
Chase <chase.leimbach@selu.edu>
2022-11-19 02:44:50 -0600

    Frontend Visuals
    Few adjustments across site

--- Web/LearningStarter/learning-starter-web/src/pages/landing-page/landing-page.tsx
+++ Web/LearningStarter/learning-starter-web/src/pages/landing-page/landing-page.tsx
@@ -1,13 +1,18 @@
-import React from "react";
-import { Header } from "semantic-ui-react";
+import React, { Component } from "react";
+import { Header, Button, Progress } from "semantic-ui-react";
 import "./landing-page.css";
+import abstract from "../../assets/abstract.png";
+import { render } from "@testing-library/react";
 
 //This is a basic Component, and since it is used inside of
 //'../../routes/config.tsx' line 31, that also makes it a page
 export const LandingPage = () => {
   return (
     <div className="home-page-container">
-      <Header>Home Page</Header>
+      <div className="home-items">
+        <Header className="header">Coming Soon to Casinos Near You!</Header>
+        <img className="abstract-img" src={abstract} width={750} height={500} ></img>
+      </div>
     </div>
   );
 };
